c9dc4716a5533e6d7e94fbbe9e68bfc4702e269c
gem00n <o.ghanaiem@student.aaup.edu>
2022-07-15 20:56:23 +0300

    38

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -3,13 +3,21 @@ import './ExpenseItem.css';
 
 
 function ExpenseItem (props) {
-    // const expenseDate = new Date(2021, 2 , 28); 
-    // const expenseTitle = 'Car Insurance';
-    // const expenseAmount = 294.67;
+    
+    const day = props.date.toLocaleString('en-US' , {day: '2-digit'});
+    const month = props.date.toLocaleString('en-US' , {month: 'long'});
+    const year = props.date.getFullYear();
 
     return (
     <div className="expense-item">
-   <div>{props.date.toISOString()}</div> 
+
+   <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+
+
+   </div> 
 
    <div className="expense-item__description">
     <h2>{props.title}</h2>
