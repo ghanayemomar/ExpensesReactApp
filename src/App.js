d6153480c78cbf840a1e0529117c310104932146
gem00n <o.ghanaiem@student.aaup.edu>
2022-07-15 22:16:50 +0300

    39

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from './Components/Expenses'
 
 function App() {
   const expenses = [
@@ -23,33 +23,10 @@ function App() {
     },
   ];
 
+
   return (
    <div>
-    <h1>Let's Get Started</h1>
-    <ExpenseItem
-    title ={expenses[0].title}
-    amount={expenses[0].amount}
-    date={expenses[0].date}
-    ></ExpenseItem>
-
-    <ExpenseItem
-    title ={expenses[1].title}
-    amount={expenses[1].amount}
-    date={expenses[1].date}
-    ></ExpenseItem>
-
-    <ExpenseItem
-    title ={expenses[2].title}
-    amount={expenses[2].amount}
-    date={expenses[2].date}
-    ></ExpenseItem>
-
-    <ExpenseItem
-    title ={expenses[3].title}
-    amount={expenses[3].amount}
-    date={expenses[3].date}
-    ></ExpenseItem>
-  
+    <Expenses expenses={expenses}/>
    </div>
    
   );
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,36 +1,22 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-
-
-function ExpenseItem (props) {
-    
-    const day = props.date.toLocaleString('en-US' , {day: '2-digit'});
-    const month = props.date.toLocaleString('en-US' , {month: 'long'});
-    const year = props.date.getFullYear();
-
-    return (
+function ExpenseItem(props) {
+  return (
     <div className="expense-item">
+        <ExpenseDate date={props.date}/> 
+        {/* In this way we call the component and make it recive data to view it! */}
+    
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
 
-   <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-
-
-   </div> 
-
-   <div className="expense-item__description">
-    <h2>{props.title}</h2>
-
-    <div className="expense-item__price">${props.amount}</div>
-   </div>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
-    );
+  );
 }
 export default ExpenseItem;
 
-
-
-//THIS IS OUR COMPONET!!!!!!!!! which we want to re-use and show it many times!!! but with diff Data!!! 
-//out function recivied props from the app function and we for real it recivied many on props (diffrent data every time) and when we run the code it will be shown like stand-alone! 
+//THIS IS OUR COMPONET!!!!!!!!! which we want to re-use and show it many times!!! but with diff Data!!!
+//out function recivied props from the app function and we for real it recivied many on props (diffrent data every time) and when we run the code it will be shown like stand-alone!
 //and that our main goal is to re-use our component with diffrent data
