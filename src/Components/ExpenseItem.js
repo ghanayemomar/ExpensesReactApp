d00cf0a9e47fe102d964c42436213a750bc35e87
gem00n <o.ghanaiem@student.aaup.edu>
2022-07-15 20:04:43 +0300

    37

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,19 +1,20 @@
 import './ExpenseItem.css';
 
 
-function ExpenseItem () {
-    const expenseDate = new Date(2021, 2 , 28); 
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
+
+function ExpenseItem (props) {
+    // const expenseDate = new Date(2021, 2 , 28); 
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 294.67;
 
     return (
     <div className="expense-item">
-   <div>{expenseDate.toISOString()}</div> 
+   <div>{props.date.toISOString()}</div> 
 
    <div className="expense-item__description">
-    <h2>{expenseTitle}</h2>
+    <h2>{props.title}</h2>
 
-    <div className="expense-item__price">${expenseAmount}</div>
+    <div className="expense-item__price">${props.amount}</div>
    </div>
     </div>
     );
@@ -21,6 +22,7 @@ function ExpenseItem () {
 export default ExpenseItem;
 
 
-// in {}: <- we can type an simple JS code 
-// new Date: is an JS build-in function which takes Date's.
-// we cannot assign date alone we have to add an ISOString(An JS built in function) to assign date to String
+
+//THIS IS OUR COMPONET!!!!!!!!! which we want to re-use and show it many times!!! but with diff Data!!! 
+//out function recivied props from the app function and we for real it recivied many on props (diffrent data every time) and when we run the code it will be shown like stand-alone! 
+//and that our main goal is to re-use our component with diffrent data
