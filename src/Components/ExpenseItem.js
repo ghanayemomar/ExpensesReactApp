b7d3b72f8affb03bcd1d1807a3b9d994f10bcef1
gem00n <o.ghanaiem@student.aaup.edu>
2022-07-15 18:19:11 +0300

    36

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -2,16 +2,25 @@ import './ExpenseItem.css';
 
 
 function ExpenseItem () {
-    
+    const expenseDate = new Date(2021, 2 , 28); 
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
     return (
     <div className="expense-item">
-   <div>March 28th 2021</div>
+   <div>{expenseDate.toISOString()}</div> 
+
    <div className="expense-item__description">
-    <h2>Car Insurance</h2>
-    <div className="expense-item__price">$294.67</div>
+    <h2>{expenseTitle}</h2>
+
+    <div className="expense-item__price">${expenseAmount}</div>
    </div>
     </div>
     );
 }
-
 export default ExpenseItem;
+
+
+// in {}: <- we can type an simple JS code 
+// new Date: is an JS build-in function which takes Date's.
+// we cannot assign date alone we have to add an ISOString(An JS built in function) to assign date to String
