2fd5fa52f8efbb01431b77fd2beb2df883305fbc
gem00n <o.ghanaiem@student.aaup.edu>
2022-07-17 04:43:20 +0300

    49

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React , {useState} from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card';
-
 const  ExpenseItem = (props) => {
 
-  
-let title = props.title
+const [title,setTitle]  = useState(props.title);
+
 
 const clickHandler = (props) => {
- title = "New Title";
+ setTitle('Updated');
 
   };
 
